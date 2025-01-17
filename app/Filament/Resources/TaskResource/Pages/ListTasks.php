3310ec8d09e0f3ff6cca06596870a7ced25cca1a
nyraareda <nyraareda3@gmail.com>
2025-01-17 19:13:20 +0200

    add badge beside tab

--- app/Filament/Resources/TaskResource/Pages/ListTasks.php
+++ app/Filament/Resources/TaskResource/Pages/ListTasks.php
@@ -4,14 +4,23 @@ namespace App\Filament\Resources\TaskResource\Pages;
 
 use App\Enums\TaskStatus;
 use App\Filament\Resources\TaskResource;
+use App\Models\Task;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Resources\Pages\ListRecords\Tab;
+use Illuminate\Support\Collection;
 
 
 class ListTasks extends ListRecords
 {
     protected static string $resource = TaskResource::class;
+    public Collection $orderByStatuses;
+    public function __construct()
+    {
+        $this->orderByStatuses = Task::select('status', \DB::raw('count(*) as task_count'))
+            ->groupBy('status')
+            ->pluck('task_count', 'status');
+    }
 
     protected function getHeaderActions(): array
     {
@@ -23,14 +32,17 @@ class ListTasks extends ListRecords
     {
         return [
           'pending'=> Tab::make(label: 'Pending Tasks')
+              ->badge(badge: $this->orderByStatuses[TaskStatus::Pending->value] ??'0')
             ->modifyQueryUsing(function ($query) {
                 return $query->where('status', TaskStatus::Pending->value);
             }),
             'completed'=> Tab::make(label: 'Completed Tasks')
+                ->badge(badge: $this->orderByStatuses[TaskStatus::Completed->value] ??'0')
             ->modifyQueryUsing(function ($query) {
                 return $query->where('status', TaskStatus::Completed->value);
             }),
             'all'=> Tab::make(label: 'All Tasks')
+            ->badge(badge: $this->orderByStatuses->sum())
         ];
     }
 }
